f7a2d88cac701f847fe75464a19843dcd0746501
kel-elKapitan <kelly.c.mcgowan80@gmail.com>
2021-10-24 16:13:53 +0100

    created a dataframe of the strongest pokemon

--- pokemon.py
+++ pokemon.py
@@ -2,15 +2,10 @@
 
 # I would like a list of less than 50 pokemon
 
-
 import pandas as pd
 import numpy as pandas
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
-
-
 
 # import the data
 
@@ -20,17 +15,19 @@ print(raw.describe())
 
 # create a ist of columns that tell how strong the pokemon we are using fares against other types of pokemon
 strong_against = ['against_bug','against_dark', 'against_dragon','against_electric', 'against_fairy', 'against_fight', 'against_fire','against_flying', 'against_ghost', 'against_grass', 'against_ground','against_ice', 'against_normal', 'against_poison', 'against_psychic','against_rock', 'against_steel', 'against_water']
+the_strongest = []
 
 # print all pokemon in their retrospective group against_<type>
 for i in strong_against:
     strongest = raw[raw[i] == 4]
 
+    the_strongest.append(strongest)
     print('Pokemon strong ' + i.upper() + ' types')
     print(strongest['name'])
     print('-----------')
     print('\n')
 
-
-
+# dataset of the strongest pokemon against other pokemon saved in the_strongest
+print(the_strongest)
 
 
