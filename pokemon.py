d9795bd415e52ed28cda9cb9a1e25352bb730de8
kel-elKapitan <kelly.c.mcgowan80@gmail.com>
2021-10-24 16:06:23 +0100

    Added comments

--- pokemon.py
+++ pokemon.py
@@ -18,12 +18,14 @@ raw = pd.read_csv('pokemon.csv')
 print(raw.columns)
 print(raw.describe())
 
+# create a ist of columns that tell how strong the pokemon we are using fares against other types of pokemon
 strong_against = ['against_bug','against_dark', 'against_dragon','against_electric', 'against_fairy', 'against_fight', 'against_fire','against_flying', 'against_ghost', 'against_grass', 'against_ground','against_ice', 'against_normal', 'against_poison', 'against_psychic','against_rock', 'against_steel', 'against_water']
 
+# print all pokemon in their retrospective group against_<type>
 for i in strong_against:
     strongest = raw[raw[i] == 4]
 
-    print('Pokemon strong against ' + i.upper() + ' types')
+    print('Pokemon strong ' + i.upper() + ' types')
     print(strongest['name'])
     print('-----------')
     print('\n')
